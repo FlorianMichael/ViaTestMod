2e0e70640a38c398c70750c6ec04ca9e70ab0a3e
Nassim Jahnke <nassim@njahnke.dev>
2024-04-07 11:06:28 +0200

    Print data component type on error and stop reading

--- src/main/java/com/viaversion/viatestmod/mixin/MixinComponentChanges_2.java
+++ src/main/java/com/viaversion/viatestmod/mixin/MixinComponentChanges_2.java
@@ -1,6 +1,5 @@
 package com.viaversion.viatestmod.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.viaversion.viatestmod.ViaTestMod;
 import it.unimi.dsi.fastutil.objects.Reference2ObjectArrayMap;
 import it.unimi.dsi.fastutil.objects.Reference2ObjectMap;
@@ -9,11 +8,10 @@ import net.minecraft.component.DataComponentType;
 import net.minecraft.network.RegistryByteBuf;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.Optional;
 
-@Mixin(targets = "net/minecraft/component/ComponentChanges$2")
+@Mixin(targets = "net/minecraft/component/ComponentChanges$1")
 public class MixinComponentChanges_2 {
 
     /**
@@ -29,21 +27,22 @@ public class MixinComponentChanges_2 {
         } else {
             Reference2ObjectMap<DataComponentType<?>, Optional<?>> reference2ObjectMap = new Reference2ObjectArrayMap(i + j);
 
-            try {
-                int k;
-                DataComponentType dataComponentType;
-                for(k = 0; k < i; ++k) {
-                    dataComponentType = (DataComponentType)DataComponentType.PACKET_CODEC.decode(registryByteBuf);
+            int k;
+            DataComponentType dataComponentType;
+            for(k = 0; k < i; ++k) {
+                dataComponentType = (DataComponentType)DataComponentType.PACKET_CODEC.decode(registryByteBuf);
+                try {
                     Object object = dataComponentType.getPacketCodec().decode(registryByteBuf);
                     reference2ObjectMap.put(dataComponentType, Optional.of(object));
+                } catch (Exception e) {
+                    ViaTestMod.LOGGER.error("Failed to decode component changes for type {}", dataComponentType);
+                    throw e;
                 }
+            }
 
-                for(k = 0; k < j; ++k) {
-                    dataComponentType = (DataComponentType)DataComponentType.PACKET_CODEC.decode(registryByteBuf);
-                    reference2ObjectMap.put(dataComponentType, Optional.empty());
-                }
-            } catch (Exception e) {
-                ViaTestMod.LOGGER.error("Failed to decode component changes", e);
+            for(k = 0; k < j; ++k) {
+                dataComponentType = (DataComponentType)DataComponentType.PACKET_CODEC.decode(registryByteBuf);
+                reference2ObjectMap.put(dataComponentType, Optional.empty());
             }
 
             return new ComponentChanges(reference2ObjectMap);
