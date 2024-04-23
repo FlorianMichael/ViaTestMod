e276580cb47ac887fc06cb97f038055f5c2f7341
Nassim Jahnke <nassim@njahnke.dev>
2024-04-23 20:03:29 +0200

    Add mixin to bypass server resource packs

---
+++ src/main/java/com/viaversion/viatestmod/mixin/MixinClientCommonNetworkHandler.java
@@ -0,0 +1,29 @@
+package com.viaversion.viatestmod.mixin;
+
+import net.minecraft.client.network.ClientCommonNetworkHandler;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.network.packet.c2s.common.ResourcePackStatusC2SPacket;
+import net.minecraft.network.packet.s2c.common.ResourcePackSendS2CPacket;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientCommonNetworkHandler.class)
+public abstract class MixinClientCommonNetworkHandler {
+
+    @Shadow
+    @Final
+    protected ClientConnection connection;
+
+    @Inject(method = "onResourcePackSend", at = @At("HEAD"), cancellable = true)
+    public void blockPack(ResourcePackSendS2CPacket packet, CallbackInfo ci) {
+        if (true) return; // TODO Add toggle
+        this.connection.send(new ResourcePackStatusC2SPacket(packet.id(), ResourcePackStatusC2SPacket.Status.ACCEPTED));
+        this.connection.send(new ResourcePackStatusC2SPacket(packet.id(), ResourcePackStatusC2SPacket.Status.DOWNLOADED));
+        this.connection.send(new ResourcePackStatusC2SPacket(packet.id(), ResourcePackStatusC2SPacket.Status.SUCCESSFULLY_LOADED));
+        ci.cancel();
+    }
+}
